1804d67adb31f8c37d51246c79f15baf4892dc4b
yi.wu <yi.wu@baifendian.com>
2017-03-07 11:50:26 +0800

    seperate get_data container, for later update

--- quickstart/init_container/get_data.sh
+++ quickstart/init_container/get_data.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -eu
-
 function get_data() {
   out_dir=${JOB_PATH}"/"${JOB_NAME}
   split_count=$SPLIT_COUNT
@@ -37,23 +35,4 @@ function get_data() {
   echo "Done."
 }
 
-function start_train() {
-  jobconfig=${JOB_PATH}"/"${JOB_NAME}"/"${TRAIN_CONFIG_DIR}
-  cd /root
-  cp -rf $jobconfig/* .
-
-  python /root/start_paddle.py \
-    --dot_period=10 \
-    --ports_num=$CONF_PADDLE_PORTS_NUM \
-    --ports_num_for_sparse=$CONF_PADDLE_PORTS_NUM_SPARSE \
-    --log_period=50 \
-    --num_passes=10 \
-    --trainer_count=$TRAINER_COUNT \
-    --saving_period=1 \
-    --local=0 \
-    --config=trainer_config.lr.py \
-    --use_gpu=0
-}
-
-get_data && \
-start_train
+get_data
